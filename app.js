c103edc183de6199537fab2e4eb98c6810f5f168
sandiagung43231675323412 <sandiagung4323@gmail.com>
2023-03-24 13:58:23 +0700

    change port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req,res)=>{
     res.send({message: 'Hello World !!!'})
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port, ()=>{
     console.log(`your application running http://localhost:${port}`)
 })
